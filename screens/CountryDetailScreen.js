76265f1076276e822ee8ae1c381d7b7d632e6878
Ivan Zhelev <nvjcompany@gmail.com>
2020-02-02 12:15:41 +0200

    Details View

--- screens/CountryDetailScreen.js
+++ screens/CountryDetailScreen.js
@@ -1,19 +1,49 @@
 import React, {useState, useEffect} from 'react';
-import {ScrollView, ActivityIndicator, Text} from 'react-native';
+import {ScrollView, ActivityIndicator, Alert, Text} from 'react-native';
 import API from '../remote';
 import Layout from '../layout/Layout';
 
 
 const ContryDetails = (props) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [data, setData] = useState({});
+    const [error, setError] = useState({
+        hasError: false,
+        message: null
+    });
+
     const alpha2Code = props.navigation.getParam('alpha2Code');
+
+    useEffect(()=>{
+        API
+            .get(`alpha1/${alpha2Code}`)
+            .then(response=>{
+                setData(response.data);
+            })
+            .catch(error=>{
+                // Alert.alert('Error', 'Error occurred!');
+                setError({
+                    hasError: true,
+                    message: 'Error occurred!'
+                });
+            })
+            .finally(()=>{
+                setTimeout(()=>{
+                    setIsLoading(false);
+                }, 2000)
+            });
+    }, []);
 
 
     return (
         <Layout>
-            <ScrollView>
-                <Text>It works!</Text>
-                <Text>{alpha2Code}</Text>
-            </ScrollView>
+            {isLoading && <ActivityIndicator size="large" />}
+            {!isLoading &&
+                <ScrollView>
+                    <Text>{data.name}</Text>
+                    <Text>{data.capital}</Text>
+                </ScrollView>
+            }
         </Layout>
     );
 };
